27f110e8d2cc7dbf6864d6843cd57a19cac7e4ea
Walker Knapp <walkerknapp3@gmail.com>
2020-02-18 10:53:35 -0500

    Resolved CUDA build conflicts

---
+++ binvox/src/main/java/com/walker/jvox/Binvox.java
@@ -0,0 +1,13 @@
+package com.walker.jvox;
+
+import java.nio.file.Path;
+
+public class Binvox {
+    public static Voxels parseBinvox(Path binvoxPath) {
+        return null;
+    }
+
+    public static void generateBinvox(Voxels voxels, Path binvoxPath) {
+
+    }
+}
